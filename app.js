6f6378d3f43d6d140b1136b4220f1b919bbbabe8
HenrikH <henrik.hellem@live.com>
2023-01-31 17:25:27 +0100

    Solved lab

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.static(__dirname + "/public"))
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/home.html")
+})
+
+app.get("/about", (req, res) => {
+    res.sendFile(__dirname + "/views/about.html")
+})
+
+app.get("/works", (req, res) => {
+    res.sendFile(__dirname + "/views/works.html")
+})
+
+app.get("/photo-gallery", (req, res) => {
+    res.sendFile(__dirname + "/views/photo-gallery.html")
+})
+
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`)
+})
